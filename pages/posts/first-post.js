6aca05f3a29abd54ad4db02e32c3a4beee2a2716
c9u11 <tjrdud6412@naver.com>
2023-12-03 23:55:59 +0900

    Image
    - Next Image

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function FirstPost() {
@@ -7,6 +8,7 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+      <Image src='/images/sunny.jpeg' alt='sunny' width={1153 / 3} height={1440 / 3} />
     </>
   )
 }
