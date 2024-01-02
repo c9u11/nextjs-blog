c10989cd7cc43e7969c80f561e568afdf6864d7a
c9u11 <tjrdud6412@naver.com>
2024-01-03 01:11:37 +0900

    ✨ Get Notion Databases
    - intergration 가이드에 따라 private key 생성
    - notionhq/client 설치
    - databases query를 틍해 목록 가져오기
    ! 목록에 대한 제목 등 상세 정보가 없음 !

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,28 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
-import Link from 'next/link';
-import Date from '../components/date';
+import { Client } from '@notionhq/client';
 
 // getStaticProps : static generation
 // getServerSideProps : server-side rendering
 // useSWR : client-side rendering
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
+  const notion = new Client({
+    auth: process.env.NEXT_PUBLIC_NOTION_TOKEN,
+  });
+
+  const response = await notion.databases.query({
+    database_id: '9105f127b6b740e2a8d38688da6b31d2',
+  })
   return {
     props: {
-      allPostsData,
+      notionData: response,
     },
   };
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ notionData }) {
+  console.log(notionData);
   return (
     <Layout home>
       <Head>
@@ -33,7 +38,7 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {/* {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>{title}</Link>
               <br />
@@ -41,7 +46,7 @@ export default function Home({ allPostsData }) {
                 <Date dateString={date} />
               </small>
             </li>
-          ))}
+          ))} */}
         </ul>
       </section>
     </Layout>
