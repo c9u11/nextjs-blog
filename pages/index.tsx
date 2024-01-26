97d90ccb717f709a50bc87d2875758e87f57956c
c9u11 <tjrdud6412@naver.com>
2024-01-26 10:13:57 +0900

    🚸 ServerSide Rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,12 @@ import { getDatabase } from '../api/notion';
 // getStaticProps : static generation
 // getServerSideProps : server-side rendering
 // useSWR : client-side rendering
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const database = await getDatabase('9105f127b6b740e2a8d38688da6b31d2');
   return {
     props: {
-      posts: database.results,
-    },
+      posts: database.results
+    }
   };
 }
 
@@ -24,7 +24,12 @@ export default function Home({ posts }) {
       <section>
         <PostList>
           {posts?.map(({ id, properties, last_edited_time }) => (
-            <PostItem key={id} id={id} title={properties.Name.title[0].plain_text} lastEditedTime={last_edited_time} />
+            <PostItem
+              key={id}
+              id={id}
+              title={properties.Name.title[0].plain_text}
+              lastEditedTime={last_edited_time}
+            />
           ))}
         </PostList>
       </section>
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -6,12 +6,12 @@ import { PostItem, PostList } from '../../components/post';
 // getStaticProps : static generation
 // getServerSideProps : server-side rendering
 // useSWR : client-side rendering
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const database = await getDatabase('9105f127b6b740e2a8d38688da6b31d2');
   return {
     props: {
-      posts: database.results,
-    },
+      posts: database.results
+    }
   };
 }
 
@@ -20,7 +20,12 @@ export default function Posts({ posts }) {
     <Layout>
       <PostList>
         {posts?.map(({ id, properties, last_edited_time }) => (
-          <PostItem key={id} id={id} title={properties.Name.title[0].plain_text} lastEditedTime={last_edited_time} />
+          <PostItem
+            key={id}
+            id={id}
+            title={properties.Name.title[0].plain_text}
+            lastEditedTime={last_edited_time}
+          />
         ))}
       </PostList>
     </Layout>
